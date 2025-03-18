f7ae5bbbb4aea0fb2bd9c1f1481c02b58aea95ea
Sagar9455 <yuva.vidyasagar@gmail.com>
2025-03-18 10:55:18 +0530

    Sagar

--- btn_oled_9.py
+++ btn_oled_9.py
@@ -240,14 +240,14 @@ try:
             selected_sequence.append(BTN_FIRST)
             b = str(variable)
             display_text(b)
-            time.sleep(0.5)
+            #time.sleep(0.2)
 
         if GPIO.input(BTN_SECOND) == GPIO.LOW:
             variable=(variable*10)+2
             selected_sequence.append(BTN_SECOND)
             a = str(variable)
             display_text(a)
-            time.sleep(0.5)      
+            #time.sleep(0.2)      
 
         if GPIO.input(BTN_ENTER) == GPIO.LOW:
             varFinal=variable
@@ -258,15 +258,18 @@ try:
             display_text(f"{selected_option}")
 
             if selected_option == "ECU Information":
+                time.sleep(0.5)
+                display_text("Fetching\nECU Information...")
                 get_ecu_information()
+                display_text("Completed")
             if selected_option == "Exit":
                 os.system("exit")
             selected_sequence.clear()  # Reset sequence after confirmation
-            time.sleep(1)
+            #time.sleep(0.1)
 
         if GPIO.input(BTN_THANKS) == GPIO.LOW:
             display_text("Shutting Down")
-            time.sleep(0.2)
+            time.sleep(0.1)
             os.system('sudo poweroff')
 
         time.sleep(0.1)
